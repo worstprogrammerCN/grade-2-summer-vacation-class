c972dd585e1ea4061b8b4c496730d2daee68d74e
worstprogrammerCN <657278923@qq.com>
2017-07-20 13:34:50 +0800

    准确率93左右

--- project/sub.py
+++ project/sub.py
@@ -1,17 +1,24 @@
 ## import modules here 
 import helper
 import submission
-
 from sklearn.metrics import f1_score
+from sklearn.model_selection import train_test_split
+from sklearn import linear_model
 
 training_data = helper.read_data('./asset/training_data.txt')
 classifier_path = './asset/classifier.dat'
-submission.train(training_data, classifier_path)
+#submission.train(training_data, classifier_path)
 
-test_data = helper.read_data('./asset/tiny_test.txt')
-prediction = submission.test(test_data, classifier_path)
-print(prediction)      
+#test_data = helper.read_data('./asset/tiny_test.txt')
+#prediction = submission.test(test_data, classifier_path)
+#print(prediction)  
+data = helper.read_data('./asset/training_data.txt')
+X, Y = submission.preprocess(data)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.33, random_state=42)
+clf = linear_model.SGDClassifier()
+clf.fit(X_train, Y_train)
+prediction = clf.predict(X_test)
 
-from sklearn.metrics import f1_score
-ground_truth = [1, 1, 2, 1]
-print(f1_score(ground_truth, prediction, average='micro'))
+print(f1_score(Y_test, prediction, average='micro'))
+
+input("wait for exit......")
--- project/submission.py
+++ project/submission.py
@@ -1,15 +1,48 @@
+import re
+from sklearn.model_selection import train_test_split
+from sklearn import linear_model
+
 ################# preprocessing #################
 def preprocess(unprocessed_data):
-    
-    data = unprocessed_data
-    for line in data:
-        line = [1, 2]
-    return data
+    X = []
+    Y = []
+    count = 0
+    for line in unprocessed_data:
+        pos_primary_stress = -1
+        parts = re.split("\s|:", line)
+        word = parts[0]
+        vowels = []
+        consonants = []
+        primary_stress = ""
+        for index in range(1, len(parts)):
+            if re.match('[a-zA-Z]+\d', parts[index]): #若是vowel则尾部必带有数字
+                vowels.append(parts[index])
+            else:
+                consonants.append(parts[index])
+            if parts[index].find('1') != -1: #若含有'1'则是primary_stress
+                primary_stress = parts[index]
+        if count < 4:
+            print(parts)
+            count += 1
+        len_stresses = len(parts[1:])
+        is_first_vowel_primary = int(primary_stress == vowels[0])
+        pos_primary_stress_in_vowel = vowels.index(primary_stress)
+        #if pos_primary_stress == 0:
+            #print("这个单词", word, "的首个发音为pri_stress")
+        x = [len(word), len_stresses, len(vowels), len(consonants), pos_primary_stress, is_first_vowel_primary]
+        X.append(x)
+        Y.append(pos_primary_stress_in_vowel)
+    return X, Y
 ################# training #################
 
 def train(data, classifier_file):# do not change the heading of the function
-    data = preprocess(data)
-    print(data[:4])
+    X, Y = preprocess(data)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.33, random_state=42)
+    clf = linear_model.SGDClassifier()
+    clf.fit(X_train, Y_train)
+    fo = open(classifier_file, "w")
+    fo.write(clf)
     pass # **replace** this line with your code    
 
 ################# testing #################
