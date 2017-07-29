e144ad4fec6f66cfbebc9f91ebdff8a178d742cb
worstprogrammerCN <657278923@qq.com>
2017-07-30 01:35:36 +0800

    创建新的分支

--- project/sub.py
+++ project/sub.py
@@ -8,7 +8,7 @@ from sklearn import linear_model
 training_data = helper.read_data('./asset/training_data.txt')
 classifier_path = './asset/classifier.dat'
 #submission.train(training_data, classifier_path)
-
+#
 #test_data = helper.read_data('./asset/tiny_test.txt')
 #prediction = submission.test(test_data, classifier_path)
 #print(prediction)  
